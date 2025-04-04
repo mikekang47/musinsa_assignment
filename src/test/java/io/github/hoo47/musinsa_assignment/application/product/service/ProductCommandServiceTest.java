69352de7c1804b7405c018a47a0660faa6d88414
mikekang47 <sihookang47@gmail.com>
2025-04-05 02:58:08 +0900

    fix: ProductCommandServiceTest

--- src/test/java/io/github/hoo47/musinsa_assignment/application/product/service/ProductCommandServiceTest.java
+++ src/test/java/io/github/hoo47/musinsa_assignment/application/product/service/ProductCommandServiceTest.java
@@ -1,10 +1,10 @@
 package io.github.hoo47.musinsa_assignment.application.product.service;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-
-import java.math.BigDecimal;
-
+import io.github.hoo47.musinsa_assignment.application.product.dto.request.ProductCreateRequest;
+import io.github.hoo47.musinsa_assignment.common.exception.BusinessErrorCode;
+import io.github.hoo47.musinsa_assignment.common.exception.BusinessException;
+import io.github.hoo47.musinsa_assignment.domain.product.Product;
+import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,10 +12,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
-import io.github.hoo47.musinsa_assignment.application.product.dto.request.ProductCreateRequest;
-import io.github.hoo47.musinsa_assignment.common.exception.BusinessErrorCode;
-import io.github.hoo47.musinsa_assignment.common.exception.BusinessException;
-import io.github.hoo47.musinsa_assignment.domain.product.Product;
+import java.math.BigDecimal;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @SpringBootTest
 @ActiveProfiles("test")
@@ -29,14 +29,14 @@ class ProductCommandServiceTest {
     @DisplayName("상품을 생성할 수 있다")
     void createProduct() {
         // given
-        ProductCreateRequest request = new ProductCreateRequest("A", 1L, BigDecimal.valueOf(10000));
+        ProductCreateRequest request = new ProductCreateRequest(1L, 1L, BigDecimal.valueOf(10000));
 
         // when
         Product product = productCommandService.createProduct(request);
 
         // then
         assertThat(product).isNotNull();
-        assertThat(product.getBrand()).isEqualTo("A");
+        assertThat(product.getBrand().getName()).isEqualTo("A");
         assertThat(product.getPrice()).isEqualTo(BigDecimal.valueOf(10000));
         assertThat(product.getCategory().getName()).isEqualTo("상의");
     }
@@ -45,23 +45,35 @@ class ProductCommandServiceTest {
     @DisplayName("존재하지 않는 카테고리로 상품을 생성할 수 없다")
     void createProduct_CategoryNotFound() {
         // given
-        ProductCreateRequest request = new ProductCreateRequest("A", 999L, BigDecimal.valueOf(10000));
+        ProductCreateRequest request = new ProductCreateRequest(1L, 999L, BigDecimal.valueOf(10000));
 
         // when & then
         assertThatThrownBy(() -> productCommandService.createProduct(request))
-            .isInstanceOf(BusinessException.class)
-            .hasFieldOrPropertyWithValue("errorCode", BusinessErrorCode.CATEGORY_NOT_FOUND);
+                .isInstanceOf(BusinessException.class)
+                .hasFieldOrPropertyWithValue("errorCode", BusinessErrorCode.CATEGORY_NOT_FOUND);
+    }
+
+    @Test
+    @DisplayName("존재하지 않는 브랜드로 상품을 생성할 수 없다")
+    void createProduct_BrandNotFound() {
+        // given
+        ProductCreateRequest request = new ProductCreateRequest(999L, 1L, BigDecimal.valueOf(10000));
+
+        // when & then
+        assertThatThrownBy(() -> productCommandService.createProduct(request))
+                .isInstanceOf(BusinessException.class)
+                .hasFieldOrPropertyWithValue("errorCode", BusinessErrorCode.BRAND_NOT_FOUND);
     }
 
     @Test
     @DisplayName("상품 가격은 0보다 커야 한다")
     void createProduct_InvalidPrice() {
         // given
-        ProductCreateRequest request = new ProductCreateRequest("A", 1L, BigDecimal.ZERO);
+        ProductCreateRequest request = new ProductCreateRequest(1L, 1L, BigDecimal.ZERO);
 
         // when & then
         assertThatThrownBy(() -> productCommandService.createProduct(request))
-            .isInstanceOf(BusinessException.class)
-            .hasFieldOrPropertyWithValue("errorCode", BusinessErrorCode.INVALID_PRICE);
+                .isInstanceOf(BusinessException.class)
+                .hasFieldOrPropertyWithValue("errorCode", BusinessErrorCode.INVALID_PRICE);
     }
 }
