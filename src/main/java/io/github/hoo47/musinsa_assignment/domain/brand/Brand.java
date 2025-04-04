c3b34115fb3e9971982a8df2e54737b9371b16e6
mikekang47 <sihookang47@gmail.com>
2025-04-05 02:34:33 +0900

    feat: create Brand and BrandRepository

---
+++ src/main/java/io/github/hoo47/musinsa_assignment/domain/brand/Brand.java
@@ -0,0 +1,25 @@
+package io.github.hoo47.musinsa_assignment.domain.brand;
+
+import io.github.hoo47.musinsa_assignment.domain.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "brands")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Brand extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    public Brand(String name) {
+        this.name = name;
+    }
+} 
---
+++ src/main/java/io/github/hoo47/musinsa_assignment/domain/brand/BrandRepository.java
@@ -0,0 +1,6 @@
+package io.github.hoo47.musinsa_assignment.domain.brand;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BrandRepository extends JpaRepository<Brand, Long> {
+} 
