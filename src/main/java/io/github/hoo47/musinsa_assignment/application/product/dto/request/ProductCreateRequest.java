15c087da6789f2542ee00792c93dd74f37352aa5
mikekang47 <sihookang47@gmail.com>
2025-04-05 02:37:00 +0900

    feat: change brand to brandId in ProductCreateRequest

--- src/main/java/io/github/hoo47/musinsa_assignment/application/product/dto/request/ProductCreateRequest.java
+++ src/main/java/io/github/hoo47/musinsa_assignment/application/product/dto/request/ProductCreateRequest.java
@@ -1,19 +1,18 @@
 package io.github.hoo47.musinsa_assignment.application.product.dto.request;
 
-import java.math.BigDecimal;
-
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 
+import java.math.BigDecimal;
+
 public record ProductCreateRequest(
-    @NotBlank(message = "브랜드명은 필수입니다")
-    String brand,
+        @NotNull(message = "브랜드 ID는 필수입니다")
+        Long brandId,
 
-    @NotNull(message = "카테고리 ID는 필수입니다")
-    @Positive(message = "카테고리 ID는 0보다 커야 합니다")
-    Long categoryId,
+        @NotNull(message = "카테고리 ID는 필수입니다")
+        Long categoryId,
 
-    @NotNull(message = "가격은 필수입니다")
-    BigDecimal price
-) {} 
+        @NotNull(message = "가격은 필수입니다")
+        BigDecimal price
+) {
+}
