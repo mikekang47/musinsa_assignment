e2aa8810051c6e6c315b769912625f69f5f35cbd
mikekang47 <sihookang47@gmail.com>
2025-04-05 02:36:09 +0900

    feat: implement create product method and add errorCode for brand not found

--- src/main/java/io/github/hoo47/musinsa_assignment/application/product/service/ProductCommandService.java
+++ src/main/java/io/github/hoo47/musinsa_assignment/application/product/service/ProductCommandService.java
@@ -1,18 +1,19 @@
 package io.github.hoo47.musinsa_assignment.application.product.service;
 
-import java.math.BigDecimal;
-
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
 import io.github.hoo47.musinsa_assignment.application.product.dto.request.ProductCreateRequest;
 import io.github.hoo47.musinsa_assignment.common.exception.BusinessErrorCode;
 import io.github.hoo47.musinsa_assignment.common.exception.BusinessException;
+import io.github.hoo47.musinsa_assignment.domain.brand.Brand;
+import io.github.hoo47.musinsa_assignment.domain.brand.BrandRepository;
 import io.github.hoo47.musinsa_assignment.domain.category.Category;
 import io.github.hoo47.musinsa_assignment.domain.category.CategoryRepository;
 import io.github.hoo47.musinsa_assignment.domain.product.Product;
 import io.github.hoo47.musinsa_assignment.domain.product.ProductRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
 
 @Service
 @Transactional
@@ -21,16 +22,24 @@ public class ProductCommandService {
 
     private final ProductRepository productRepository;
     private final CategoryRepository categoryRepository;
+    private final BrandRepository brandRepository;
 
     public Product createProduct(ProductCreateRequest request) {
         Category category = categoryRepository.findById(request.categoryId())
                 .orElseThrow(() -> new BusinessException(BusinessErrorCode.CATEGORY_NOT_FOUND));
 
+        Brand brand = brandRepository.findById(request.brandId())
+                .orElseThrow(() -> new BusinessException(BusinessErrorCode.BRAND_NOT_FOUND));
+
         if (request.price().compareTo(BigDecimal.ZERO) <= 0) {
             throw new BusinessException(BusinessErrorCode.INVALID_PRICE);
         }
 
-        Product product = new Product(request.price(), category, request.brand());
+        var product = Product.builder()
+                .price(request.price())
+                .category(category)
+                .brand(brand)
+                .build();
         return productRepository.save(product);
     }
 }
--- src/main/java/io/github/hoo47/musinsa_assignment/common/exception/BusinessErrorCode.java
+++ src/main/java/io/github/hoo47/musinsa_assignment/common/exception/BusinessErrorCode.java
@@ -12,6 +12,9 @@ public enum BusinessErrorCode {
     // Category
     CATEGORY_NOT_FOUND("CATEGORY-001", "존재하지 않는 카테고리입니다."),
 
+    // Brand
+    BRAND_NOT_FOUND("BRAND-001", "존재하지 않는 브랜드입니다."),
+
     // Product
     INVALID_PRICE("PRODUCT-001", "상품 가격은 0보다 커야 합니다."),
     PRODUCT_NOT_FOUND("PRODUCT-002", "존재하지 않는 상품입니다.");
