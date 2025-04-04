adf5dea7241626fd98f8fafcb6dd48dd45902aac
hoo47 <hoo@channel.io>
2025-04-04 21:11:42 +0900

    feat: init project

---
+++ src/main/java/io/github/hoo47/musinsa_assignment/MusinsaAssignmentApplication.java
@@ -0,0 +1,13 @@
+package io.github.hoo47.musinsa_assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusinsaAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusinsaAssignmentApplication.class, args);
+    }
+
+}
