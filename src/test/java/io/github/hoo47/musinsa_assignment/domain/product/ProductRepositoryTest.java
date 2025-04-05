3c9c18bbc3ec9fe4217fc53c11099c1f595f07d5
mikekang47 <sihookang47@gmail.com>
2025-04-06 01:51:03 +0900

    feat: inject categoryRepository, brandRepository in ProductRepositoryTest

--- src/test/java/io/github/hoo47/musinsa_assignment/domain/product/ProductRepositoryTest.java
+++ src/test/java/io/github/hoo47/musinsa_assignment/domain/product/ProductRepositoryTest.java
@@ -1,11 +1,17 @@
 package io.github.hoo47.musinsa_assignment.domain.product;
 
+import io.github.hoo47.musinsa_assignment.domain.brand.Brand;
+import io.github.hoo47.musinsa_assignment.domain.brand.BrandRepository;
+import io.github.hoo47.musinsa_assignment.domain.category.Category;
+import io.github.hoo47.musinsa_assignment.domain.category.CategoryRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -17,6 +23,32 @@ class ProductRepositoryTest {
     @Autowired
     private ProductRepository productRepository;
 
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Autowired
+    private BrandRepository brandRepository;
+
+    @BeforeEach
+    void setUp() {
+        // 카테고리 생성
+        Category category1 = categoryRepository.save(Category.builder()
+                .name("상의")
+                .build());
+
+
+        // 브랜드 생성
+        Brand brand1 = brandRepository.save(Brand.builder()
+                .name("A")
+                .build());
+
+        productRepository.save(Product.builder()
+                .price(BigDecimal.valueOf(10000))
+                .category(category1)
+                .brand(brand1)
+                .build());
+    }
+
     @Test
     @DisplayName("모든 상품을 조회할 수 있다")
     void findAllTest() {
@@ -27,7 +59,6 @@ class ProductRepositoryTest {
         assertThat(products).isNotEmpty();
         assertThat(products.size()).isGreaterThan(0);
 
-        // 브랜드별로 카테고리마다 1개씩, 총 9개 브랜드 * 8개 카테고리 = 72개 상품이 있어야 함
-        assertThat(products.size()).isEqualTo(72);
+        assertThat(products.size()).isEqualTo(1);
     }
 } 
