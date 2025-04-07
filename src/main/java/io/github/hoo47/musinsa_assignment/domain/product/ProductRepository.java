3af91ea1182ad9fd133c105412a1ce723c1035df
mikekang47 <sihookang47@gmail.com>
2025-04-08 03:59:06 +0900

    feat: add findTop1ByCategoryIdOrderByPriceAsc method test: add its testcase

--- src/main/java/io/github/hoo47/musinsa_assignment/domain/product/ProductRepository.java
+++ src/main/java/io/github/hoo47/musinsa_assignment/domain/product/ProductRepository.java
@@ -3,16 +3,8 @@ package io.github.hoo47.musinsa_assignment.domain.product;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
-    @Query("""
-            SELECT p FROM Product p
-            JOIN FETCH p.category c
-            JOIN FETCH p.brand b
-            WHERE c.id = :categoryId
-            ORDER BY b.name
-            """)
-    List<Product> findByCategoryWithFetch(@Param("categoryId") Long categoryId);
+
+    List<Product> findTop1ByCategoryIdOrderByPriceAsc(Long categoryId);
 }
