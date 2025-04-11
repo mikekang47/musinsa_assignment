39e3da8c4c55044fdf14c27a95f6a4ce8a871083
mikekang47 <sihookang47@gmail.com>
2025-04-11 19:24:07 +0900

    feat: enhance GlobalExceptionHandler with logging and general exception handling

--- src/main/java/io/github/hoo47/musinsa_assignment/web/common/GlobalExceptionHandler.java
+++ src/main/java/io/github/hoo47/musinsa_assignment/web/common/GlobalExceptionHandler.java
@@ -1,26 +1,35 @@
 package io.github.hoo47.musinsa_assignment.web.common;
 
-import java.util.HashMap;
-import java.util.Map;
-
+import io.github.hoo47.musinsa_assignment.common.exception.BusinessErrorCode;
+import io.github.hoo47.musinsa_assignment.common.exception.BusinessException;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import io.github.hoo47.musinsa_assignment.common.exception.BusinessErrorCode;
-import io.github.hoo47.musinsa_assignment.common.exception.BusinessException;
-import jakarta.validation.ConstraintViolation;
-import jakarta.validation.ConstraintViolationException;
+import java.util.HashMap;
+import java.util.Map;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(BusinessException.class)
     public ResponseEntity<ErrorResponse> handleBusinessException(BusinessException e) {
+        log.error("Error occured", e);
         ErrorResponse response = ErrorResponse.of(e.getErrorCode());
         return new ResponseEntity<>(response, e.getErrorCode().getHttpStatus());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleGeneralException(Exception e) {
+        log.error("Unexpected error occurred", e);
+        ErrorResponse response = ErrorResponse.of(BusinessErrorCode.INTERNAL_SERVER_ERROR);
+        return new ResponseEntity<>(response, BusinessErrorCode.INTERNAL_SERVER_ERROR.getHttpStatus());
     }
 
     @ExceptionHandler(ConstraintViolationException.class)
