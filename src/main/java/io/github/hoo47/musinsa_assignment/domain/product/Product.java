1d21c573f297194b39433bdb7095767d84811a0c
mikekang47 <sihookang47@gmail.com>
2025-04-11 17:56:23 +0900

    feat: add unique indexes for brand and category entities to improve query performance

--- src/main/java/io/github/hoo47/musinsa_assignment/domain/product/Product.java
+++ src/main/java/io/github/hoo47/musinsa_assignment/domain/product/Product.java
@@ -1,18 +1,37 @@
 package io.github.hoo47.musinsa_assignment.domain.product;
 
+import java.math.BigDecimal;
+
 import io.github.hoo47.musinsa_assignment.domain.BaseTimeEntity;
 import io.github.hoo47.musinsa_assignment.domain.brand.Brand;
 import io.github.hoo47.musinsa_assignment.domain.category.Category;
-import jakarta.persistence.*;
-import lombok.*;
-
-import java.math.BigDecimal;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Index;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
-@Table(name = "products")
+@Table(
+    name = "products",
+    indexes = {
+        @Index(name = "idx_product_price", columnList = "price"),
+        @Index(name = "idx_product_category_price", columnList = "category_id, price"),
+        @Index(name = "idx_product_brand_category", columnList = "brand_id, category_id")
+    }
+)
 @Builder
 public class Product extends BaseTimeEntity {
 
