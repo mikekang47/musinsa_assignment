77c2dc3e7bc0b2634c7e86f960badddeb2470bf3
sihoo kang <gyeong0308@gmail.com>
2025-04-11 19:48:34 +0900

    Fix/redis (#23)
    * feat: add initial data for categories, brands, and products
    
    * feat: enhance GlobalExceptionHandler with logging and general exception handling
    
    * feat: update build configuration to set Java source compatibility and add Jackson JSR310 module
    
    * feat: enhance CacheConfig with custom ObjectMapper and improved cache error handling
    
    * feat: add endpoint to retrieve brand with the lowest total price and remove unused endpoint from CategoryController
    
    * feat: add internal server error code and handle empty category and product cases in CategoryProductPriceUsecase
    
    * feat: implement lowest price brand retrieval in BrandController and add corresponding test cases
    
    * feat: add global exception handling for MethodArgumentNotValidException
    
    * feat: configure Docker setup with Redis and application service
    
    * feat: update README to reflect new Docker setup and API endpoint changes
    
    * refactor: clean up unused imports in controller and use case classes
    
    ---------
    
    Co-authored-by: mikekang47 <sihookang47@gmail.com>

--- src/main/java/io/github/hoo47/musinsa_assignment/controller/v1/brand/BrandController.java
+++ src/main/java/io/github/hoo47/musinsa_assignment/controller/v1/brand/BrandController.java
@@ -5,25 +5,39 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.fge.jsonpatch.JsonPatch;
 import com.github.fge.jsonpatch.JsonPatchException;
-import io.github.hoo47.musinsa_assignment.application.brand.dto.request.BrandUpdateRequest;
-import io.github.hoo47.musinsa_assignment.application.brand.service.BrandCommandService;
 import io.github.hoo47.musinsa_assignment.application.brand.dto.request.BrandCreateRequest;
+import io.github.hoo47.musinsa_assignment.application.brand.dto.request.BrandUpdateRequest;
 import io.github.hoo47.musinsa_assignment.application.brand.dto.response.BrandResponse;
+import io.github.hoo47.musinsa_assignment.application.brand.service.BrandCommandService;
+import io.github.hoo47.musinsa_assignment.application.product.dto.response.BrandProductSummaryResponse;
+import io.github.hoo47.musinsa_assignment.application.usecase.BrandLowestPriceUsecase;
 import io.github.hoo47.musinsa_assignment.common.exception.BusinessErrorCode;
 import io.github.hoo47.musinsa_assignment.common.exception.BusinessException;
 import io.github.hoo47.musinsa_assignment.domain.brand.Brand;
 import jakarta.validation.Valid;
-import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/api/v1/brands")
 @RestController
-@RequiredArgsConstructor
 public class BrandController {
 
     private final BrandCommandService brandCommandService;
-    private final ObjectMapper objectMapper = new ObjectMapper();
+    private final BrandLowestPriceUsecase brandLowestPriceUsecase;
+    private final ObjectMapper objectMapper;
+
+    public BrandController(BrandCommandService brandCommandService, BrandLowestPriceUsecase brandLowestPriceUsecase) {
+        this.brandCommandService = brandCommandService;
+        this.brandLowestPriceUsecase = brandLowestPriceUsecase;
+        this.objectMapper = new ObjectMapper() ;
+    }
+
+
+
+    @GetMapping("/lowest-price")
+    public BrandProductSummaryResponse getLowestPriceBrand() {
+        return brandLowestPriceUsecase.getBrandWithLowestTotalPrice();
+    }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
